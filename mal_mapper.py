629e1e4fe4606778e4242e6ac8d6f56a2c6dc88e
ryandash <56605453+ryandash@users.noreply.github.com>
2026-03-21 16:54:23 -0400

    Update mal_mapper.py
    - Fix getting known mappings after change

--- mal_mapper.py
+++ mal_mapper.py
@@ -283,13 +283,13 @@ def load_existing_malids(category: str) -> dict[str, int]:
     for file in dirpath.glob("*.json"):
         try:
             data = safe_load_json(file)
-            mal_id = data.get("MalId")
-            if not mal_id:
+            mal_ids = data.get("MalIds")
+            if not mal_ids:
                 continue
             series_id = file.stem
-            existing_lookup[series_id] = int(mal_id)
+            existing_lookup[series_id] = int(mal_ids[0])
         except Exception as e:
-            print(f"[WARN] Skipping {file.name} — no valid MAL ID ({e})")
+            print(f"[WARN] Skipping {file.name} — invalid MAL IDs ({e})")
 
     return existing_lookup
 
